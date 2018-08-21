8e18a2396e72c5a45e9279244352b5f9a5aa8553
harryscholes <harryscholes@gmail.com>
2018-08-21 16:00:13 +0100

    Add initial code

---
+++ src/LSMH.jl
@@ -0,0 +1,9 @@
+module LSMH
+
+using Base.Iterators
+
+const UInt64_Max = typemax(UInt64)-1
+
+include("hashing.jl")
+
+end  # module
---
+++ src/hashing.jl
@@ -0,0 +1,132 @@
+struct MinHash{U<:Unsigned}
+    minhash::U
+    kmer::String
+end
+
+"""
+    minhash(sequence::String, k::Int, [seed<:UInt])
+
+Split `sequence` into `k`-shingles and hash using a `seed`ed hash function.
+"""
+function minhash(sequence::String, k::Int, seed::UInt=zero(UInt))
+    mh = UInt64_Max
+
+    for i = 1:length(sequence)-k
+        ch = hash(SubString(sequence, i:i+k-1), UInt(seed))
+
+        if ch < mh
+            mh = ch
+        end
+    end
+
+    mh
+end
+
+minhash(sequence::String, k::Int, seed::Int) = minhash(sequence, k, UInt(seed))
+
+"""
+"""
+function minhash_with_sequence(sequence::String, k::Int, seed::UInt=zero(UInt))
+    mh = UInt64_Max
+    seq = nothing
+
+    for i = 1:length(sequence)-shingle_length
+        shingle = SubString(sequence, i:i+shingle_length-1)
+        ch = hash(shingle, UInt(seed))
+
+        if ch < mh
+            mh = ch
+            seq = shingle
+        end
+    end
+
+    MinHash(mh, string(seq))
+end
+
+"""
+    signature(sequence::String, k::Int, n_hashes::Int)
+
+Split `sequence` into `k`-shingles and hash using `n_hashes` hash functions.
+"""
+function signature(sequence::String, k::Int, n_hashes::Int)
+    A = Vector{UInt64}(1:n_hashes)
+
+    for seed in 1:n_hashes
+        A[seed] = minhash(sequence, k, seed)
+    end
+
+    A
+end
+
+"""
+    lsh(A, n_bands)
+
+Hashes signatures into buckets. For a signature matrix `A` of
+(n_hashes,n_sequences), each signature is split into `n_bands` and hashed.
+"""
+function lsh(A::AbstractArray{T,2}, n_bands::Int) where T<:Unsigned
+    n_hashes = size(A)[1]
+    step = Int(n_hashes/n_bands)
+
+    hashtable = [Dict{T,Set{Int64}}() for _ in 1:n_bands]
+
+    for col in 1:size(A)[2]
+        for (band, ptr) in zip(1:n_bands, 1:step:n_hashes)
+            h = hash(view(A, ptr:ptr+step-1, col))
+
+            if haskey(hashtable[band], h)
+                push!(hashtable[band][h], col)
+            else
+                hashtable[band][h] = Set([col])
+            end
+        end
+    end
+
+    hashtable
+end
+
+"""
+    candidates(hashtable)
+
+Return candidate matches.
+"""
+function candidates(hashtable)
+    A = Vector{Tuple}()
+
+    for band in hashtable
+        for (bucket, ids) in pairs(band)
+            if length(ids) > 1
+                for (i, j) in product(ids, ids)
+                    i == j && continue
+                    push!(A, (i,j))
+                end
+            end
+        end
+    end
+
+    A
+end
+
+"""
+    jaccard(a, b)
+
+Calculate Jaccard similarity of two sets.
+"""
+jaccard(a::T, b::T) where T<:AbstractArray = sum(a .== b) / length(a)
+
+"""
+    filter_candidates(signatures, candidates, threshold)
+
+Remove `candidates` with Jaccard similarity between their `signatures` less than
+`threshold`.
+"""
+function filter_candidates(signatures, candidates, threshold)
+    out = typeof(candidates)()
+
+    for (i, j) in candidates
+        similarity = jaccard(signatures[:,i], signatures[:,j])
+        similarity >= threshold ? push!(out, (i,j)) : nothing
+    end
+
+    out
+end
