d8a47ed95bef12e206dc144cae565781fd0df9ca
harryscholes <harryscholes@gmail.com>
2018-08-25 18:25:25 +0100

    Test strings of length 1:100

--- test/murmurhash3.jl
+++ test/murmurhash3.jl
@@ -5,8 +5,8 @@ function MurmurHash3_x86_32(s::String, seed::UInt32)
 end
 
 @testset "Test agreement of Julia and C implementations of MurmurHash3_x86_32" begin
-    for seed in rand(UInt, 100)
-        for l in 25:25:100
+    for seed in rand(UInt, 10)
+        for l in 1:100
             for _ = 1:100
                 s = randstring(l)
                 seed = UInt32(0)
