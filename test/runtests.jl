d0329708f90151946db2c16618a206de467a7357
harryscholes <harryscholes@gmail.com>
2018-09-11 16:14:06 +0100

    Add proteins.jl tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using LSMH
-using Test, Random
+using Test, Random, BioSequences
 
 @testset "LSMH" begin
 
 include("murmurhash3.jl")
 include("xxhash.jl")
 include("hashing.jl")
+include("proteins.jl")
 
 end
